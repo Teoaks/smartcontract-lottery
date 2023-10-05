c07e4d2099507065c1ca6b54d982f5768b4873b5
teodorlinux <ikanovic.teodor@gmail.com>
2023-10-05 10:39:15 +0200

    lottery is up

---
+++ README.md
@@ -0,0 +1,23 @@
+# Proveably Random Raffle Contract
+
+## About
+
+This code is to create a proveably random smart contract lottery.
+
+## What we want it to do?
+
+1. Users can enter by paying an entrance ticket
+    1. Fees from the entrance pool are going to the winner during the draw
+2. After X period of time, the lottery will automatically draw a winner
+    1. And this will be done programatically
+3. Using Chainlinnk VRF & Chainlink Automation 
+    1. Chainlink VRF -> Randomness
+    2. Chainlink Automation -> Time based trigger 
+
+## Tests!
+
+1. Write some deploy scripts
+2. Write our tests
+   1. Work on a local chain
+   2. Forked Testnet
+   3. Forked Mainnet
---
+++ test/integration/DeployRaffleTest.t.sol
@@ -0,0 +1,42 @@
+// Layout of Contract:
+// version
+// imports
+// errors
+// interfaces, libraries, contracts
+// Type declarations
+// State variables
+// Events
+// Modifiers
+// Functions
+
+// Layout of Functions:
+// constructor
+// receive function (if exists)
+// fallback function (if exists)
+// external
+// public
+// internal
+// private
+// internal & private view & pure functions
+// external & public view & pure functions
+
+//SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.18;
+
+import {DeployRaffle} from "../../script/DeployRaffle.s.sol";
+import {Raffle} from "../../src/Raffle.sol";
+import {Test, console} from "forge-std/Test.sol";
+import {HelperConfig} from "../../script/HelperConfig.s.sol";
+import {VRFCoordinatorV2Mock} from "@chainlink/contracts/src/v0.8/mocks/VRFCoordinatorV2Mock.sol";
+import {CreateSubscription, FundSubscription, AddConsumer} from "../../script/Interactions.s.sol";
+
+contract DeployRaffleTest is Test {
+    function createTheSubscription() public view {}
+
+    function fundTheSubscription() public view {}
+
+    function startingTheBroadcast() public view {}
+
+    function addingTheConsumer() public view {}
+}
